9dd46c1245f399ae707563d5fa5d8073156af915
Jake Ash and Joseph Tubridy <joseph@>
2020-02-13 09:33:28 -0800

    add a cat picture to the help queue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TicketList from './TicketList';
 import Header from './Header';
+import Picture from './Picture';
 
 function App() {
   return (
     <div>
       <Header />
       <TicketList />
+      <Picture />
     </div>
   );
 }
